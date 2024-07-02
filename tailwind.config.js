2c5b958de33553530ed3f6e4b09ae6c38d398777
Momen Nasr <147791027+EngMomenNasr@users.noreply.github.com>
2024-07-02 03:29:47 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'tajawal': ['Tajawal','sans-serif']
+      },
+      colors: {
+        color: {
+          1: "#AD8A74",
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
